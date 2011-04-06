7a45f41876a8905b9298bd4f04e3b3aebe25fb42
Nathaniel Jones <nathaniel@pointeractive.com>
2011-04-06 11:07:13 -0500

    Expand Railing Subdomain abilities

--- lib/railings/domains.rb
+++ lib/railings/domains.rb
@@ -1,18 +1,36 @@
 module Railings
   module Domains
-    MAPPINGS   = YAML::load_file Rails.root.join 'config', 'domains.yml' rescue false
-    MAPPINGS ||= { }
+    YAML::load_file(Rails.root.join 'config', 'domains.yml').tap do |mappings|
+      mappings.each do |domain, mapping|
+        save domain, Subdomain.new(domain, mapping)
+      end
+    end rescue nil
 
-    def self.const_missing domain
-      Class.new do
-        @domain = domain.downcase.to_s
-        class << self
-          def matches? request
-            request.subdomain.include? subdomain
-          end
-
-          def subdomain
-            MAPPINGS.has_key?(@domain) ? MAPPINGS[@domain] : @domain
+    class Subdomain < String
+      def initialize name, mapping = nil
+        replace (mapping || name).to_s.downcase
+      end
+      
+      def matches? request
+        subdomain == request.subdomain
+      end
+    end
+    
+    class << self
+      def const_missing name
+        save name, Subdomain.new(name)
+      end
+      
+      def save name, subdomain
+        const_set(name, subdomain).tap do
+          ApplicationController.send :helper, Module.new do
+            define_method "url_for_#{name.downcase}" do |options|
+              unless subdomain == request.subdomain
+                options[:host]      = with_subdomain subdomain
+                options[:only_path] = false
+              end
+              options
+            end
           end
         end
       end
